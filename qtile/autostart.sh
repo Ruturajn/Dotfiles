0e0c42bd54199e3300930fd8903b1616151b351c
Ruturajn <nanotiruturaj@gmail.com>
2022-07-05 16:53:48 +0530

    `update` : qtile/autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -6,7 +6,7 @@
 # feh --bg-scale <Path_To_Dotfiles>/Dotfiles/Wallpapres/5120x2880.jpg
 
 ## Using nitrogen to set wallpaper on startup
-nitrogen --restore &
+nitrogen --restore
 
 ## Starting compton compositor on startup for transparency
 # If transparency is not working, try with vsync
