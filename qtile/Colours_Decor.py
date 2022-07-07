3cb35c14cd9f3f88e277a58e70366d372e06820c
Ruturajn <nanotiruturaj@gmail.com>
2022-07-07 16:03:33 +0530

    `update` : Colours_Decor.py to better handle colorschemes

--- qtile/Colours_Decor.py
+++ qtile/Colours_Decor.py
@@ -1,40 +1,14 @@
 # from typing import List  # noqa: F401
-# 
+
 # from libqtile import layout, hook, bar
 # from qtile_extras import widget
 # from qtile_extras.bar import Bar
 # from libqtile.config import Click, Drag, Group, Key, Match, Screen
 # from libqtile.lazy import lazy
-from qtile_extras.widget.decorations import RectDecoration, BorderDecoration
+from qtile_extras.widget.decorations import RectDecoration#, BorderDecoration
 
 # Import the necessary libraries
 import json, os, glob
-
-# Search for the colorscheme file and read it.
-file_name = glob.glob(os.path.expanduser("~/.config/wpg/schemes/*.json"))
-file = open(file_name[0])
-data = json.load(file)
-
-# Create and empty list that will house the colours from the current 
-# colorscheme.
-color_list = []
-
-# Iterate over the colours, and store them in the list.
-for color in data["colors"]:
-    color_list.append(data["colors"][color])
-
-color_list.reverse()
-
-foreground_colour = data["special"]["background"]
-#foreground_colour = "#FFFFFF"
-#foreground_colour = "#000000"
-
-#foreground_colour_icon = "#000000"
-#foreground_colour_icon = "#FFFFFF"
-foreground_colour_icon = data["special"]["foreground"]
-
-# Close the file.
-file.close()
 
 colors_1 = ["#600060", "#992A88", "#758CCA", "#C1F4B6", ""]
 
@@ -52,19 +26,45 @@ colors_7 = ["#ffa630", "#d7e8ba", "#4da1a9", "#2e5077", "#611c35"]
 
 colors = ["#d1ccdc", "#424c55", "#f5edf0", "#886f68", "#3d2c2e", "#533a7b"]
 
-colors_dracula = ["#6272a4", "#8be9fd", "#50fa7b",
-                  "#ffb86c", "#ff79c6", "#bd93f9", "#ff5555", "#f1fa8c"]
+colors_dracula = ["#44475a", "#f8f8f2", "#6272a4", "#8be9fd", "#50fa7b", "#ffb86c",
+                  "#ff79c6", "#bd93f9", "#ff5555", "#f1fa8c"]
 
 colors_catpuccin = ["#F2CDCD", "#DDB6F2", "#F5C2E7", "#E8A2AF", "#F28FAD", "#F8BD96",
                     "#FAE3B0", "#ABE9B3", "#B5E8E0", "#96CDFB", "#89DCEB"]
 
-foreground_colour = "#000000"
-#foreground_colour = "#FFFFFF"
+# Search for the colorscheme file and read it.
+file_name = glob.glob(os.path.expanduser("~/.config/wpg/schemes/*.json"))
+try:
+    file = open(file_name[0])
+    data = json.load(file)
 
-#foreground_colour_icon = "#000000"
-foreground_colour_icon = "#FFFFFF"
+    # Create and empty list that will house the colours from the current 
+    # colorscheme.
+    color_list = []
 
-color_theme = color_list
+    # Iterate over the colours, and store them in the list.
+    for color in data["colors"]:
+        color_list.append(data["colors"][color])
+
+    color_list.reverse()
+
+    foreground_colour = data["special"]["background"]
+    foreground_colour_icon = data["special"]["foreground"]
+    
+    # Change the color theme
+    color_theme = color_list
+
+    # Close the file.
+    file.close()
+except:
+    # foreground_colour = "#FFFFFF"
+    foreground_colour = "#000000"
+
+    # foreground_colour_icon = "#000000"
+    foreground_colour_icon = "#FFFFFF"
+
+    color_theme = colors_catpuccin
+
 
 decor_layout = {
     "decorations": [
