a9d082cdec70eb2a45b262dc37330d0f16e08d1f
Ruturajn <nanotiruturaj@gmail.com>
2023-01-01 20:24:12 -0500

    update: alacritty and nvim plugin configs

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -75,7 +75,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['rust_analyzer'].setup {
   capabilities = capabilities
